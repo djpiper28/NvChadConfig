cdb488517f0f60d866c29cc2b0a03dc1ad53598b
Danny Piper <djpiper28@gmail.com>
2023-09-25 11:32:51 +0100

    Update plugins.lua

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -4,13 +4,12 @@ local plugins = {
     {"andweeb/presence.nvim", lazy = false, },
     {"sbdchd/neoformat", lazy = false, },
     {"akinsho/bufferline.nvim", opts = {
-         enforce_regular_tabs = true,
-         always_show_bufferline = true,
       },
       lazy = false,
     },
     {"lervag/vimtex", lazy = false, },
     {"salkin-mada/openscad.nvim", lazy = false, },
+    {"github/copilot.vim", lazy = false, }
 }
 
 return plugins
