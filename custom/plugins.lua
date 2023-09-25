ef24f394119c80025b106c3fb08a33e048794bb5
Danny Piper <djpiper28@gmail.com>
2023-09-25 15:25:29 +0100

    Update plugins.lua

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -1,15 +1,47 @@
----@type NvPluginSpec[]
+--@type NvPluginSpec[]
 local plugins = {
-    {"wakatime/vim-wakatime", lazy = false, },
-    {"andweeb/presence.nvim", lazy = false, },
-    {"sbdchd/neoformat", lazy = false, },
-    {"akinsho/bufferline.nvim", opts = {
+  { "wakatime/vim-wakatime", lazy = false },
+  { "andweeb/presence.nvim", lazy = false },
+  { "sbdchd/neoformat", lazy = false },
+  { "akinsho/bufferline.nvim", opts = {}, lazy = false },
+  { "lervag/vimtex", lazy = false },
+  { "salkin-mada/openscad.nvim", lazy = false },
+  { "github/copilot.vim", lazy = false },
+  { "williamboman/mason.nvim", lazy = false },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      automatic_installation = true,
+      ensure_installed = {
+        "bashls",
+        "clangd",
+        "cmake",
+        "csharp_ls",
+        "cssls",
+        "custom_elements_ls",
+        "docker_compose_language_service",
+        "dockerls",
+        "eslint",
+        "gopls",
+        "gradle_ls",
+        "helm_ls",
+        "html",
+        "jsonls",
+        "ltex",
+        "lua_ls",
+        "openscad_lsp",
+        "rust_analyzer",
+        "sqlls",
+        "tailwindcss",
+        "tsserver",
+        "vimls",
+        "vuels",
+        "yamlls",
       },
-      lazy = false,
     },
-    {"lervag/vimtex", lazy = false, },
-    {"salkin-mada/openscad.nvim", lazy = false, },
-    {"github/copilot.vim", lazy = false, }
+    lazy = false,
+  },
+  { "neovim/nvim-lspconfig", lazy = false },
 }
 
 return plugins
