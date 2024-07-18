e160f5ffc4cd386b01e49624b20d47445b4a510f
Danny Piper <djpiper28@gmail.com>
2024-07-18 15:45:15 +0100

    feat: added mapping

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -4,6 +4,7 @@ M.neotest = {
   n = {
     ["<leader>lt"] = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%')) <cr>", "Run tests" },
     ["<leader>ly"] = { "<cmd>:Neoformat <cr>", "Run Formatter" },
+    ["<leader>lo"] = { "<cmd>lua require('neotest').output_panel.open() <cr>", "Test output" }
   },
 }
 
